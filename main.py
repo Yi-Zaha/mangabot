23f561308761bd3af1e1fe7ee6bce5168472202d
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-07-18 12:24:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ uvloop.install()
 from pathlib import Path
 from bot import bot, manga_updater
 from models import DB 
+from models import DBX
 from extras import load_plugin
 
 load_plugin(Path("extras.py"))
 
 async def async_main():
     db = DB()
+    dbx = DBX()
     await db.connect()
+    await dbx.connect()
     
 if __name__ == '__main__':
     loop = aio.get_event_loop_policy().get_event_loop()
